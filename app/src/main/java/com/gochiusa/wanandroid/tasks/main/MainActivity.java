5160c291801d5703bfe4cc02ee70674d0c785003
Liu Zhengjie <1023192250@qq.com>
2020-07-14 23:05:42 +0800

    feat:初步敲定刚打开应用时显示的界面布局

---
+++ app/src/main/java/com/gochiusa/wanandroid/tasks/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.gochiusa.wanandroid.tasks.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gochiusa.wanandroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
