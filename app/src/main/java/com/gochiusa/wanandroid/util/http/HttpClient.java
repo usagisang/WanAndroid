d9c77e19b2704790ae5cb6e5e32c8dc06ab58823
Liu Zhengjie <1023192250@qq.com>
2020-07-17 15:14:53 +0800

    feat(com.gochiusa.wanandroid.util):添加了一个新的创建连接的辅助方法

--- app/src/main/java/com/gochiusa/wanandroid/util/http/HttpClient.java
+++ app/src/main/java/com/gochiusa/wanandroid/util/http/HttpClient.java
@@ -41,7 +41,30 @@ public final class HttpClient implements Call.Factory {
     public HttpClient() {}
 
     HttpClient(Builder builder) {
+        this.mConnectionTimeout = builder.connectionTimeout;
+        this.mDoInput = builder.doInput;
+        this.mDoOutPut = builder.doOutPut;
+        this.mReadTimeout = builder.readTimeout;
+        this.mUseCache = builder.useCache;
+    }
 
+    /**
+     *  这个方法不属于OkHttp。因为整个APP实际上需要对请求进行细致设计的地方不多，
+     *  请求之间只存在URL上的差异，因此提供了这个默认的Call创建方式。
+     *  默认下，使用GET方法，无请求头。
+     * @param requestUrl 发起请求的目标域名
+     * @return 可以立刻开始进行连接的Call对象
+     */
+    public static Call createDefaultCall(String requestUrl) {
+        // 不使用建造者，直接创建，使用默认的连接设置
+        HttpClient httpClient = new HttpClient();
+        Request.Builder requestBuilder = new Request.Builder();
+        // 生成请求URL，设置在请求中
+        requestBuilder.setUrl(requestUrl);
+        // 配置剩余的请求信息
+        Request newArticleRequest = requestBuilder.get().build();
+        // 创建Call并返回
+        return httpClient.newCall(newArticleRequest);
     }
 
     /**
